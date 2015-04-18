8ae2c600a9414b85e0ff5405c00ac4dc3e46add0
Matthew Field <fieldm58@gmail.com>
2015-04-18 16:49:50 +0000

    Init

---
+++ sendudp.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+import socket, sys
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = str(" ".join(sys.argv[1:]))
+
+print "UDP target IP:", UDP_IP
+print "UDP target port;", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, #Internet
+                     socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
